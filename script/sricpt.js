307f4287fe458951ac42d09630bc4de16eb57eff
Samuel Yorke Aidoo <sya.aidoo@gmail.com>
2023-08-01 10:09:41 +0200

    create getComputerChoice() function

--- script/sricpt.js
+++ script/sricpt.js
@@ -1,1 +1,8 @@
-console.log("hello");
+
+function getComputerChoice(){
+    let game_plays = ["Rock","Paper","Scissors"];
+    return game_plays[Math.floor(Math.random() * 3)];
+
+}
+
+console.log(getComputerChoice());
