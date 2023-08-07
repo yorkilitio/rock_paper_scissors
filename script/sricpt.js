6399af03ebe3ad0d7f405494da1221dc56b8a3c4
Samuel Yorke Aidoo <sya.aidoo@gmail.com>
2023-08-07 13:31:35 +0200

    create function to play a single round

--- script/sricpt.js
+++ script/sricpt.js
@@ -5,4 +5,27 @@ function getComputerChoice(){
 
 }
 
-console.log(getComputerChoice());
+function playSingleRound(playerSelection, computerSelection){
+    playerSelectionUpper = playerSelection.toUpperCase();
+    computerSelectionUpper = computerSelection.toUpperCase();
+    
+    if((playerSelectionUpper === "PAPER" && computerSelectionUpper === "ROCK")||
+    (playerSelectionUpper === "ROCK" && computerSelectionUpper === "SCISSORS")||
+    (playerSelectionUpper === "SCISSORS" && computerSelectionUpper === "PAPER")){
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }else{
+        return `You lose :-( ${computerSelection} beats ${playerSelection}`;
+    }
+
+}
+
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+
+    let playerSelection = prompt("What do you choose? Rock, Paper, or Scissors?");
+    
+
+}
+
+console.log(playSingleRound("paper","rock"));
